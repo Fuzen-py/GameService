593a16a293802a98ebbf6b8422a94d807ba11b18
Fuzzy <fuzen@protonmail.com>
2017-12-22 02:42:14 -0500

    Code cleanup, improved modularity

---
+++ src/games/mod.rs
@@ -0,0 +1,14 @@
+mod cards;
+mod deck;
+mod deck_of_cards;
+
+pub mod blackjack;
+
+pub mod slot_machine;
+pub mod coin_toss;
+pub mod rps;
+pub mod gofish;
+
+pub use self::deck::{StandardDeck, StandardDeckError};
+pub use self::cards::{StandardCard, StandardCardFace, StandardCardParseError};
+pub use self::deck_of_cards::STANDARD_DECK_OF_CARDS;
